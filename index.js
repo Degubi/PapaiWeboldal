867bf3c968da76806725dd791bfec3ef9bfaff24
Degubi <gergonoorbi@gmail.com>
2025-01-15 19:08:27 +0100

    Upload files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const contentElement = document.getElementById('content');
 const routeButtons = document.getElementById('routesContainer').children;
 let currentPagePath = new URLSearchParams(window.location.search).get('page') ?? 'main';
+let slideIndex = 1;
 
 showPage(currentPagePath);
 window.history.replaceState(currentPagePath, null, `?page=${currentPagePath}`);
@@ -15,23 +16,50 @@ function showPage() {
 
     switch(currentPagePath) {
         case 'main':
-            contentElement.innerHTML = 'Epic nagy kép helye';
-            routeButtons[0].className = 'active';
+            contentElement.style.margin = '0px';
+            fetch('main.html').then(k => k.text()).then(k => contentElement.innerHTML = k);
+            routeButtons[0].className = 'active-route';
             break;
-        case 'services':
-            contentElement.innerHTML = 'Szolgáltatások leírása';
-            routeButtons[1].className = 'active';
+        case 'gallery':
+            contentElement.style.margin = '24px 16%';
+            fetch('gallery.html').then(k => k.text()).then(k => contentElement.innerHTML = k);
+            routeButtons[1].className = 'active-route';
             break;
         case 'about':
-            contentElement.innerHTML = 'Leírás helye';
-            routeButtons[2].className = 'active';
+            contentElement.style.margin = '24px 64px';
+            fetch('about.html').then(k => k.text()).then(k => contentElement.innerHTML = k);
+            routeButtons[2].className = 'active-route';
             break;
         case 'contact':
-            contentElement.innerHTML = 'Elérhetőségek helye';
-            routeButtons[3].className = 'active';
+            contentElement.style.margin = '24px';
+            fetch('contact.html').then(k => k.text()).then(k => contentElement.innerHTML = k);
+            routeButtons[3].className = 'active-route';
             break;
     }
 }
+
+function showSlide(n) {
+    const slides = document.getElementsByClassName('slide-element');
+    const dots = document.getElementsByClassName('dot');
+    const slideCount = slides.length;
+
+    if(n > slideCount) {
+        slideIndex = 1;
+    }
+
+    if(n < 1) {
+        slideIndex = slideCount;
+    }
+
+    for(let i = 0; i < slideCount; ++i) {
+        slides[i].style.display = 'none';
+        dots[i].classList.remove('active');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('active');
+}
+
 
 window.routeTo = function(pagePath) {
     contentElement.scrollTo(0, 0);
@@ -42,3 +70,6 @@ window.routeTo = function(pagePath) {
         return showPage();
     }
 };
+
+window.slideByOffset = function(n) { showSlide(slideIndex += n); };
+window.nthSlide = function(n) { showSlide(slideIndex = n); };
